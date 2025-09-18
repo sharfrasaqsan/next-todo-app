7f82860817eae23d3e7b639e94e2e489c1192dc1
sharfrasaqsan <sharfrasaqsan@gmail.com>
2025-09-18 23:46:06 +0530

    data context created

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Providers from "./providers";
+
 export const metadata = { title: "Next + Firebase Todos" };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-light">{children}</body>
+      <body className="bg-light">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
